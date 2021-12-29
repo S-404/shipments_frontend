84176519adbd5daa01fb028ee2ef9ebaa50e5801
Andrey <sudarev.andrey.i@gmail.com>
2021-12-29 14:23:43 +0300

    fix dateFormat (timezone dependency)

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 export const pad = (num, places) => String(num).padStart(places, '0');
 export const dateFormatHHMM = (date) => {
     let d = new Date(date);
-    return `${pad(d.getHours(),2)}:${pad(d.getMinutes(),2)}`
+    d.setHours(d.getHours() + d.getTimezoneOffset() / 60)
+    return `${pad(d.getHours(), 2)}:${pad(d.getMinutes(), 2)}`
 }
