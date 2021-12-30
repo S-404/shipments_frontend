b3cc886d8e8771c4b0582011d05c7566a4142d2d
Andrey <sudarev.andrey.i@gmail.com>
2021-12-30 07:19:35 +0300

    add picker form

--- src/components/pages/Picker.jsx
+++ src/components/pages/Picker.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import MyInput from "../UI/input/myInput";
+import {useFetching} from "../../hooks/useFetching";
+import ShipmentService from "../../api/ShipmentService";
+import MyLoader from "../UI/loader/myLoader";
+import '../../styles/pickerForm.scss'
 
 const Picker = () => {
+    const [orderNum, setOrderNum] = useState('')
+    const [locations, setLocations] = useState([{ORDER_NUM: '', GATE: '', PLACE: ''}])
+
+
+    const [fetchLocation, isLocationLoading, isLocationError] = useFetching(async () => {
+        const responseData = await ShipmentService.getData({
+            query: 'orders/order', ORDER_NUM: orderNum
+        });
+        setLocations(responseData);
+    });
+
+
+    useEffect(async () => {
+            if (orderNum.length === 9) {
+                await fetchLocation()
+            }
+        }
+        , [orderNum])
+
     return (
-        <div>
-            picker
+        <div className='picker-form'>
+            <MyInput
+                labeltext={orderNum.length?'put order num':null}
+                placeholder='put order num'
+                value={orderNum}
+                maxLength={9}
+                onChange={(e) => {
+                    setOrderNum(e.target.value)
+                }}/>
+            {isLocationLoading ?
+                <div className='picker-form__loader-div'><MyLoader/></div>
+                :
+                <div className='picker-form__locations'>
+                    <div className='picker-form__locations locations__order-num'>
+                        {locations.length ? `Order: ${locations[0].ORDER_NUM}` : 'not found'}
+                    </div>
+                    <div className='picker-form__locations locations__list'>
+                        {locations.map((location, index) => (
+                            <div className='locations__list list__location'
+                                 key={index + location}>
+                                {`GATE: ${location.GATE} - ${location.PLACE}`}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            }
+
+
         </div>
     );
 };
