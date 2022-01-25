ac4a1e02dbb3e6623877cb7bdfbd778e40080c4f
Andrey <sudarev.andrey.i@gmail.com>
2022-01-25 22:16:21 +0300

    upd styles

--- src/components/UI/input/myInput.jsx
+++ src/components/UI/input/myInput.jsx
@@ -7,7 +7,7 @@ const MyInput = React.forwardRef((props, ref) => {
         <div ref={ref} className={classes.myInput}>
             <input className={classes.myInput__input} {...props} />
             {
-                props.labeltext ?
+                props.labeltext && props.value?
                     <MyLabel htmlFor={classes.myInput__input} labeltext={props.labeltext}/>
                     :
                     null
