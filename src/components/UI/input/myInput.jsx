397a8a0d2a307fbe1022381de6c67dd817159277
Andrey <sudarev.andrey.i@gmail.com>
2022-01-26 18:45:50 +0300

    upd login page

--- src/components/UI/input/myInput.jsx
+++ src/components/UI/input/myInput.jsx
@@ -4,8 +4,8 @@ import classes from './myInput.module.css';
 
 const MyInput = React.forwardRef((props, ref) => {
     return (
-        <div ref={ref} className={classes.myInput}>
-            <input className={classes.myInput__input} {...props} />
+        <div className={classes.myInput}>
+            <input ref={ref}  className={classes.myInput__input} {...props} />
             {
                 props.labeltext && props.value?
                     <MyLabel htmlFor={classes.myInput__input} labeltext={props.labeltext}/>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,36 +2,91 @@ import React, {useState} from 'react';
 import MyInput from "../components/UI/input/myInput";
 import MySmallButton from "../components/UI/button/mySmallButton";
 import {useDispatch} from "react-redux";
+import showPasswordIcon from '../assets/show.svg'
+import hidePasswordIcon from '../assets/hide.svg'
+import '../styles/login.scss'
+import {isValidInput} from "../utils";
 
 const Login = () => {
-    const [userid, setUserid] = useState('user')
-    const [password,setPassword] = useState('')
-    const dispatch = useDispatch()
+
+    const dispatch = useDispatch();
+
+    const [userid, setUserid] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [passwordInput, setPasswordInput] = useState(null);
+
+
     const login = () => {
+
+        if (!userid || !password) return;
+
+
         dispatch({type: 'SET_USERID', value: userid});
         localStorage.setItem('userid', userid)
 
         dispatch({type: 'SET_AUTH', value: true});
         localStorage.setItem('auth', 'true')
     }
+    const onKeyDownPassword = (e) => {
+        if (e.key === 'Enter') {
+            login()
+        }
+    }
+    const onKeyDownUserID = (e) => {
+        if (e.key === 'Enter') {
+            passwordInput.focus()
+        }
+    }
+
+
     return (
-        <div>
-            <MyInput
-                placeholder='id'
-                maxLength={50}
-                labeltext='user id'
-                value={userid}
-                onChange={(e) => setUserid(e.target.value)}
-            />
-            <MyInput
-                labeltext='password'
-                type='password'
-                placeholder='password'
-            />
-            <br/>
-            <MySmallButton
-                onClick={login}
-                text='LOGIN'/>
+        <div className='login-form'>
+            <h1 className='login-form__header'>
+                {(process.env.REACT_APP_NAME).toUpperCase()} v{process.env.REACT_APP_VERSION}
+            </h1>
+            <div className='login-form__user-input login-form__input'>
+                <MyInput
+                    onKeyDown={(e) => onKeyDownUserID(e)}
+                    placeholder='id'
+                    maxLength={10}
+                    labeltext='user id'
+                    value={userid}
+                    onChange={(e) => {
+                        if (isValidInput(e.target.value)) setUserid(e.target.value)
+                    }}
+                />
+            </div>
+            <div className='login-form__password-input login-form__input'>
+                <MyInput
+                    ref={password_input => setPasswordInput(password_input)}
+                    onKeyDown={(e) => onKeyDownPassword(e)}
+                    placeholder='password'
+                    maxLength={16}
+                    labeltext='password'
+                    type={showPassword ? 'text' : 'password'}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                {password.length ?
+                    <div className='password-input__show-password-icon-div'>
+                        <img
+                            onClick={() => setShowPassword(!showPassword)}
+                            className='password-input__show-password-icon'
+                            alt='show/hide-pass'
+                            src={showPassword ? hidePasswordIcon : showPasswordIcon}/>
+                    </div>
+                    : null
+                }
+
+
+            </div>
+            <div className='login-form__login-button'>
+                <MySmallButton
+                    onClick={login}
+                    text='LOGIN'/>
+            </div>
+
         </div>
     );
 };
