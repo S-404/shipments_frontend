7bd36890484b7ae791176de2a9fa3186bf643fdd
Andrey <91373803+S-404@users.noreply.github.com>
2022-02-20 19:16:41 +0300

    add weight to gatePlaceForm (filling mode)

--- src/components/ShippingArea/GatePlaceForm/orderLine/OrderWeight.jsx
+++ src/components/ShippingArea/GatePlaceForm/orderLine/OrderWeight.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const OrderWeight = ({orderline}) => {
     return (
         <div className='dynamic-order-line__weight dynamic-order-line__col-2'>
-            {orderline.ORDER_WEIGHT === null ? '' : Math.round(orderline.ORDER_WEIGHT * 10) / 10}
+            {orderline.ORDER_WEIGHT === null ? '' : Math.round(orderline.ORDER_WEIGHT * 10) / 10 + 'kg'}
         </div>
     );
 };
